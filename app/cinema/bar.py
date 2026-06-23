67ab5250efd129314159dd4bcc042473b0efa33c
Your Name <denys@mate.academy>
2026-06-23 20:44:34 +0300

    Fix classes and cinema_visit according to review

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,5 +3,5 @@ from app.people.customer import Customer
 
 class CinemaBar:
     @staticmethod
-    def sell_product(customer: Customer, product: str) -> None:
+    def sell_product(product: str, customer: Customer) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,6 +1,6 @@
+from typing import List
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
-
 
 class CinemaHall:
     def __init__(self, number: int) -> None:
@@ -8,12 +8,12 @@ class CinemaHall:
 
     def movie_session(
         self,
-        movie: str,
-        customers: list[Customer],
-        cleaner: Cleaner,
+        movie_name: str,
+        customers: List[Customer],
+        cleaning_staff: Cleaner,
     ) -> None:
-        print(f'"{movie}" started in hall number {self.number}.')
+        print(f"Movie session in hall number {self.number} started.")
         for customer in customers:
-            customer.watch_movie(movie)
-        print(f'"{movie}" ended.')
-        cleaner.clean_hall(self.number)
+            customer.watch_movie(movie_name)
+        print(f"Movie session in hall number {self.number} ended.")
+        cleaning_staff.clean_hall(self.number)
