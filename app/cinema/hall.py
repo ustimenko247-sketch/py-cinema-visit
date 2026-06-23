b171e567994d2b3eead8119c1d42582a29f2cfb5
Your Name <denys@mate.academy>
2026-06-23 17:50:22 +0300

    Fix CinemaHall constructor and movie_session

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,8 +1,17 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie: str, customers: list, cleaner) -> None:
+    def movie_session(
+        self,
+        movie: str,
+        customers: list[Customer],
+        cleaner: Cleaner,
+    ) -> None:
         print(f'"{movie}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie)
